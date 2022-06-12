d5dc2ed96b3f627b7b3caae10345e10a53068e86
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-12 18:06:32 +1000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,7 +4,33 @@ from typing import Optional
 
 PlaneAPI = FastAPI()
 
+planedb = []
+
+class Plane(BaseModel):
+    id: int
+    name: str
+    price: float
+    is_plane: Optional[bool] = None
+
 @PlaneAPI.get('/')
 def read_root():
     return {"Hey!": "Welcome to PlaneAPI"}
-    
+
+@PlaneAPI.get('/getplane')
+def getplane():
+    return planedb
+
+@PlaneAPI.get('/getplane/{plane_id}')
+def get_a_plane(plane_id: int):
+    plane = plane_id - 1
+    return planedb[plane]
+
+@PlaneAPI.post("/getplane)")
+def add_plane(plane: Plane):
+    planedb.append(plane.dict())
+    return planedb[-1]  
+
+@PlaneAPI.delete("/getplane/{plane_id}")
+def delete_plane(plane_id :int):
+    planedb.pop(plane_id-1)
+    return {"Task": "Deletion successful"}
