0500116a735e0c203c88320878ba2b1c8a64dca8
Mr-Conos <76495776+Mr-Conos@users.noreply.github.com>
2022-07-05 13:37:55 -0700

    🤔 Fix erros

--- api/tools/data.py
+++ api/tools/data.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONNECTION_STRING")
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
